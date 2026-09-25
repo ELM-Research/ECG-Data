9b3c50cec90c71ba31ce1b62a5edc7ce12eb69a5
willxxy <willhan327@outlook.com>
2026-09-24 23:00:33 -0400

    pathing is incorrect

--- scripts/prepare_base.sh
+++ scripts/prepare_base.sh
@@ -1,19 +1,19 @@
 # ### PTB-XL
-# CUDA_VISIBLE_DEVICES=2 uv run src/preprocess_base.py.py \
+# CUDA_VISIBLE_DEVICES=2 uv run src/ecg_preprocess/preprocess_base.py \
 # --base src/ecg_preprocess/config/ptb_xl.yaml
 
 ## CODE15
-# uv run src/preprocess_base.py.py \
+# uv run src/ecg_preprocess/preprocess_base.py \
 # --base src/ecg_preprocess/config/code15.yaml
 
 # # ## CPSC'
-# uv run src/preprocess_base.py.py \
+# uv run src/ecg_preprocess/preprocess_base.py \
 # --base src/ecg_preprocess/config/cspsc.yaml
 
 # # ## CSN
-# uv run src/preprocess_base.py.py \
+# uv run src/ecg_preprocess/preprocess_base.py \
 # --base src/ecg_preprocess/config/csn.yaml
 
 # # ## MIMIC-IV
-uv run python src/preprocess_base.py.py \
+uv run python src/ecg_preprocess/preprocess_base.py \
 --config src/ecg_preprocess/config/mimic_iv_ecg.yaml
